6a519b78b65575090e1e631a711a4e909b870e91
matthew0122 <115114071+matthew0122@users.noreply.github.com>
2023-04-29 10:58:31 -0400

    finish header

--- src/home.js
+++ src/home.js
@@ -16,5 +16,9 @@ function createHeader(){
     title.id='title';
     title.innerHTML = "<h1>THE HAPPY PLATE</h1>";
     header.append(title);
+    const topButton = document.createElement("button");
+    topButton.innerText = "Order Now";
+    topButton.id = 'order';
+    header.append(topButton);
     content.appendChild(header);
 }
