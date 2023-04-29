025ff8a8af7d836f0b0155deb269d52eee4172f1
matthew0122 <115114071+matthew0122@users.noreply.github.com>
2023-04-29 10:43:59 -0400

    Add Logo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import './style.css';
+import Logo from './logo.png';
 const content = document.getElementById('content');
-const header = document.createElement('div');
-header.id = 'header';
-content.appendChild(header);
+createHeader();
+
+
+function createHeader(){
+    const header = document.createElement('div');
+    header.id = 'header';
+    const logo = new Image();
+    logo.src = Logo;
+    header.appendChild(logo);
+    content.appendChild(header);
+}
